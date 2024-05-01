278d4b66d0a2b6204cf1b89136c3260c1e668c3d
MauriDemasi <mauridemasi88@gmail.com>
2024-05-01 18:51:40 -0300

    other try deploy in vercel

--- src/index.js
+++ src/index.js
@@ -12,10 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 
 //Conectar a la base de datos de Mongoose
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI, {});
 const db = mongoose.connection;
 
 app.use("/books", bookRouter);
